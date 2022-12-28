4989adf4e9c6cbbe022d47848650359e11ef3ca2
Angel Zou <angel@zenblen.com>
2022-12-28 23:59:15 +0100

    "select word show alert"

--- extension/content.js
+++ extension/content.js
@@ -5,15 +5,36 @@ window.addEventListener("mouseup", handleSelection);
 var selectedText;
 
 function handleSelection() {
-  selectedText = window.getSelection().toString().replace(/\s/g, "");
+  sel = window.getSelection()
+  selectedText = sel.toString().replace(/\s/g, "");
+
+  //////////////// TO-DO ////////////////
+  // Try to figure out how to get context of a word
+  //////////////// TO-DO ////////////////
+  // ref
+  // https://stackoverflow.com/questions/4332753/how-to-get-selected-text-but-can-i-get-surrounding-context-in-javascript
+  // https://stackoverflow.com/questions/28173274/window-getselection-getrangeat0-innerhtml-without-breaking-nodes
+
+  // var selectedRange = sel.rangeCount ? sel.getRangeAt(0) : null;
+  // if (selectedRange) {
+  //     alert("Offset " + selectedRange.startOffset
+  //         + " in node " + selectedRange.startContainer.nodeName);
+  // };
+
+  // documentation refs
+  // https://developer.mozilla.org/en-US/docs/Web/API/Selection/getRangeAt
+  // https://developer.mozilla.org/en-US/docs/Web/API/Selection/rangeCount
+  // https://developer.mozilla.org/en-US/docs/Web/API/Selection
+
+  alert(selectedText);
 }
 
-chrome.runtime.onMessage.addListener(gotMessage);
+// chrome.runtime.onMessage.addListener(gotMessage);
 
-function gotMessage(message, sender, sendResponse) {
-  let msg =
-    selectedText && selectedText.length > 0
-      ? selectedText
-      : "_TextNotSelected_";
-  sendResponse({ swor: msg });
-}
+// function gotMessage(message, sender, sendResponse) {
+//   let msg =
+//     selectedText && selectedText.length > 0
+//       ? selectedText
+//       : "_TextNotSelected_";
+//   sendResponse({ swor: msg });
+// }
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,6 +1,7 @@
 let query = { active: true, currentWindow: true };
 
-let word;
+let word,
+    wordContext;
 
 chrome.tabs.query(query, gotTabs);
 function gotTabs(tabs) {
@@ -8,9 +9,8 @@ function gotTabs(tabs) {
     txt: "hello from popup",
   };
 
-  // No word has been selected yet, explain functionality to user
   chrome.tabs.sendMessage(tabs[0].id, msg, function (response) {
-    if (!response) {
+    if (!response) { 
       document.getElementById("phonetic").innerHTML = "Welcome!";
       document.getElementById("example").innerHTML =
         "Please select a word to see it in context";
@@ -18,88 +18,88 @@ function gotTabs(tabs) {
       document.getElementById("error").innerHTML = "Please select a word!";
     } else {
       let swo = response.swor;
-      swo = swo.replace(/[^a-zA-Z ]/g, "");
+      // swo = swo.replace(/[^a-zA-Z ]/g, "");
+      word = swo.replace(/[^a-zA-Z ]/g, "");
       // added a line
-      word = swo
-    //   dictionary(swo);
+      //   dictionary(swo);
       setValues();
     }
   });
 }
 
-let wordef,
-//   word,
-  phonetic,
-  pos,
-  defin,
-  example,
-  sourceurl,    // we change sourceurl into our website url
-  index = 0,
-  indlimit;
+// let wordef,
+// //   word,
+//   phonetic,
+//   pos,
+//   defin,
+//   example,
+//   sourceurl,    // we change sourceurl into our website url
+//   index = 0,
+//   indlimit;
 
-//////////////////////////
-// This sends a query to API to search word definition. 
-// We need to replace this with a request to backend for stored context.
-/////////////////////////
+// //////////////////////////
+// // This sends a query to API to search word definition. 
+// // We need to replace this with a request to backend for stored context.
+// /////////////////////////
 
-// async function dictionary(query) {
-//   let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
-//   let response = await fetch(url);
-//   wordef = await response.json();
-//   if (wordef && !wordef.title) {
-//     indlimit = wordef[0].meanings.length;
-//     word = wordef[0].word;
-//     phonetic = wordef[0].phonetic ? wordef[0].phonetic : "";
-//     // sourceurl = `https://en.wiktionary.org/wiki/${word}`;
-//     index = 0;
+// // async function dictionary(query) {
+// //   let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
+// //   let response = await fetch(url);
+// //   wordef = await response.json();
+// //   if (wordef && !wordef.title) {
+// //     indlimit = wordef[0].meanings.length;
+// //     word = wordef[0].word;
+// //     phonetic = wordef[0].phonetic ? wordef[0].phonetic : "";
+// //     // sourceurl = `https://en.wiktionary.org/wiki/${word}`;
+// //     index = 0;
 
-//     setValues();
+// //     setValues();
 
-//     if (indlimit > 1) {
-//       document
-//         .getElementById("navigatecontainer")
-//         .classList.remove("hidenavigator");
-//     }
-//   } else if (wordef.title) {
-//     document.getElementById("error").innerHTML = "⚠  " + wordef.title;
-//   }
+// //     if (indlimit > 1) {
+// //       document
+// //         .getElementById("navigatecontainer")
+// //         .classList.remove("hidenavigator");
+// //     }
+// //   } else if (wordef.title) {
+// //     document.getElementById("error").innerHTML = "⚠  " + wordef.title;
+// //   }
+// // }
+
+// document.getElementById("prev").addEventListener("click", handlePrevious);
+// document.getElementById("next").addEventListener("click", handleNext);
+
+// function handlePrevious() {
+//   index = index - 1;
+//   if (index < 0) index = indlimit - 1;
+//   setValues();
 // }
 
-document.getElementById("prev").addEventListener("click", handlePrevious);
-document.getElementById("next").addEventListener("click", handleNext);
+// function handleNext() {
+//   index = index + 1;
+//   if (index >= indlimit) index = 0;
+//   setValues();
+// }
 
-function handlePrevious() {
-  index = index - 1;
-  if (index < 0) index = indlimit - 1;
-  setValues();
-}
-
-function handleNext() {
-  index = index + 1;
-  if (index >= indlimit) index = 0;
-  setValues();
-}
-
-function setValues() {
-    defin = "whatever random definition"
-    example = "context sentences"
-//   pos = wordef[0].meanings[index].partOfSpeech;
-//   defin = wordef[0].meanings[index].definitions[0].definition;
-//   example = wordef[0].meanings[index].definitions[0].example
-//     ? wordef[0].meanings[index].definitions[0].example
-//     : null;
+// function setValues() {
+//     defin = "whatever random definition"
+//     example = "context sentences"
+// //   pos = wordef[0].meanings[index].partOfSpeech;
+// //   defin = wordef[0].meanings[index].definitions[0].definition;
+// //   example = wordef[0].meanings[index].definitions[0].example
+// //     ? wordef[0].meanings[index].definitions[0].example
+// //     : null;
 
 
-//   document.getElementById(
-//     "word"
-//   ).innerHTML = `${word} <a href=${sourceurl} class="searchanchor" target="_blank"><img class="searchsvg" title="read more" src = "../assets/searchonweb.svg" alt="read more"/><a>`;
+// //   document.getElementById(
+// //     "word"
+// //   ).innerHTML = `${word} <a href=${sourceurl} class="searchanchor" target="_blank"><img class="searchsvg" title="read more" src = "../assets/searchonweb.svg" alt="read more"/><a>`;
 
-    document.getElementById("word").innerHTML = `${word}`;
-//   document.getElementById("phonetic").innerHTML = `${phonetic}  (${pos})`;
-//   document.getElementById("definition").innerHTML = defin;
-  if (example) {
-    document.getElementById("example").innerHTML = `Context: ${example}`;   // replace this with our sentences
-  } else {
-    document.getElementById("example").innerHTML = "";
-  }
-}
+//     document.getElementById("word").innerHTML = `${word}`;
+// //   document.getElementById("phonetic").innerHTML = `${phonetic}  (${pos})`;
+// //   document.getElementById("definition").innerHTML = defin;
+//   if (example) {
+//     document.getElementById("example").innerHTML = `Context: ${example}`;   // replace this with our sentences
+//   } else {
+//     document.getElementById("example").innerHTML = "";
+//   }
+// }
